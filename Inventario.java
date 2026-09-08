d62ed61f1b9eca26d18a6e37d0ee32a98fbc4e5d
Dexter <josembotero17qgmail.com>
2026-09-07 21:14:50 -0500

    feat: Menu

--- Inventario.java
+++ Inventario.java
@@ -1,3 +1,24 @@
+import java.util.Scanner;
+
 public class Inventario-Noel {
-    
+    public static void main(String[] args) {
+        //
+        String[] inventario = {"Ducales","Festival","Tosh",null,null};
+        Scanner leer = new Scanner(System.in);
+        int opcion = 0;
+        //
+        while (opcion != 4) {
+            Sysout.println("===================");
+            Sysout.println("1. Listar producto");
+            Sysout.println("2. Buscar producto");
+            Sysout.println("3. Agregar producto");
+            Sysout.println("4. cerrar Menu");
+            Sysout.println("===================");
+            //
+            opcion = leer.nextInt();
+            leer.nextInt();
+            //
+            Switch (opcion) 
+        }
+    }
 }
